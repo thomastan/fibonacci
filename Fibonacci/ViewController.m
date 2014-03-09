e4d4b93a4d18e1ba155e42835d06532c43447ccd
Jerry <jey9711@gmail.com>
2014-03-09 18:12:50 -0400

    First Commit
    First Commit

--- Fibonacci/ViewController.m
+++ Fibonacci/ViewController.m
@@ -13,17 +13,61 @@
 @end
 
 @implementation ViewController
+{
+    NSArray *fibTerms;
+}
+
+-(void)fibSe{
+    NSMutableArray *fib = [[NSMutableArray alloc] init];
+    [fib addObject: [NSString stringWithFormat:@"1"]];
+    unsigned int oneAgo = 1, twoAgo = 0, current;
+    do {
+        current = oneAgo + twoAgo;
+        [fib addObject:[NSString stringWithFormat:@"%d", current]];
+        twoAgo = oneAgo, oneAgo = current;
+        
+    } while (oneAgo <= UINT_MAX - twoAgo);
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableArray *fib = [[NSMutableArray alloc] init];
+    [fib addObject: [NSString stringWithFormat:@"1"]];
+    unsigned int oneAgo = 1, twoAgo = 0, current;
+    do {
+        current = oneAgo + twoAgo;
+        [fib addObject:[NSString stringWithFormat:@"%d", current]];
+        twoAgo = oneAgo, oneAgo = current;
+    } while (twoAgo <= UINT_MAX - current - oneAgo);
+    
+    fibTerms = [NSArray arrayWithArray: fib];
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [fibTerms count];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [fibTerms objectAtIndex:indexPath.row];
+    return cell;
 }
 
 @end
